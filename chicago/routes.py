57b9c0feefa4c4b990e917306337912713dd9462
[leylairoyale] <[leylairoyale@gmail.com]>
2019-08-19 17:31:32 -0500

    SUCCESSFUL SEARCH QUERY RETURNS MATCHING ADDRESS TO THE TERMINAL!

--- chicago/routes.py
+++ chicago/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, flash
 from chicago import app
 from chicago.models import db, Address
 from chicago.forms import SearchOld, SearchNew, validators
@@ -19,47 +19,10 @@ def go_home():
     #         db.session.commit()
     return render_template("index.html", form=form, newform = newform)
 
-# @app.route("/searchold", methods=["GET", "POST"])
-# def go_search():
-#     form = SearchOld()
-#     newform = SearchNew()
-#     oldnum = SearchOld(form.old_number.data)
-#     # print(oldnum)
-#     oldq = Address.query.filter_by(old_num = oldnum).first()
-#     print(oldq)
-#     # if oldq is None:
-#     #     print("oops nah")
-#     # else:
-#     #     print("bananas for sale")
-#     return render_template("index.html", form = form, newform=newform)
 
-#below route is how to open the csvfile in a pandas dataframe
-# @app.route("/searchold", methods=["GET", "POST"])
-# def go_search():
-#     form = SearchOld()
-#     newform = SearchNew()
-#     oldnum = SearchOld(form.old_number.data)
-#     print("this is the oldnumber {}".format(oldnum))
-#     df = pd.read_csv('chicago\cacs_carmen_ave.csv')
-#         # reading = csv.DictReader(csvfile)
-#         # add_search = pd.DataFrame.from_dict(reading)
-#     print(df['old_num'][0])
-#     print(df[df['new_num'] == oldnum])
-#     for ad in df['old_num']:
-#         if ad == SearchOld(form.old_number.data):
-#             print('yes babnanas')
-#             break
-#         else:
-#             pass
-#     # testing = df[(df['old_num'] == oldnum)]
-#     # print(testing)
-#     # if str(oldnum) in str(add_search['old_num']):
-#     #     print("yes today")
-#     # else:
-#     #     print("we have no bananas today")
 
-#     return render_template("index.html", form = form, newform=newform)
 
+# below is ACCURATE for getting direction, street, num in search.
 @app.route("/searchold", methods=["GET", "POST"])
 def go_search():
     form = SearchOld()
@@ -68,30 +31,52 @@ def go_search():
     # oldnum = SearchOld(old_num=request.form.get("old_num"))
     #other thing above was querying the database too early.
     #below you have to add .data to get what was actually in the form
-    oldnum = form.old_number.data
-    print(oldnum)
-    test = Address.query.filter_by(old_num = str(oldnum)).first()
-    print(test.new_num)
-
-    #below opens a csv file and adds it row by row tot he db
-    # with open('chicago\cacs_carmen_ave.csv', newline='') as csvfile:
-    #     reading = csv.reader(csvfile)
-    #     for row in reading:
-    #         addy = Address(new_num=row[0], new_dir=row[1], new_street=row[2], old_num=row[3], old_dir=row[4], old_street=row[5], duplicate=row[6])
-    #         db.session.add(addy)
-    #         db.session.commit()
+    if form.data:
+        oldnum = form.old_number.data
+        oldstreet = form.street_name.data 
+        olddir = form.direction.data
+        question = Address.query.filter_by(old_num = str(oldnum), old_street=str(oldstreet), old_dir =str(olddir)).first()
+        if not question:
+            flash("Hmm, we don't have that address...")
+            return redirect('/')
+        else:
+            print(question.new_num, question.new_dir, question.new_street, question.duplicate)
+            return search_results(question)
     return render_template("index.html", form = form, newform=newform)
 
+
+
+    # if form.old_number.data:
+    #     oldnum = form.old_number.data
+    #     print(oldnum)
+    #     test = Address.query.filter_by(old_num = str(oldnum)).first()
+    #     if not test:
+    #         flash("Hmm, we don't have that address...")
+    #         return redirect('/')
+    #     else:
+    #         print(test.new_num)
+
+    # return render_template("index.html", form = form, newform=newform)
+
+# below is WORKING OLD SEARCH for number only.
 # @app.route("/searchold", methods=["GET", "POST"])
 # def go_search():
 #     form = SearchOld()
 #     newform = SearchNew()
-#     oldnum = SearchOld(form.old_num.data)
-#     with open('chicago\cacs_carmen_ave.csv', newline='') as csvfile:
-#         reading = csv.DictReader(csvfile)
-#         for row in reading:
-#             print(row['new_num'], row['old_num'])
-#             break
+#     #below is the error, somehow
+#     # oldnum = SearchOld(old_num=request.form.get("old_num"))
+#     #other thing above was querying the database too early.
+#     #below you have to add .data to get what was actually in the form
+#     if form.old_number.data:
+#         oldnum = form.old_number.data
+#         print(oldnum)
+#         test = Address.query.filter_by(old_num = str(oldnum)).first()
+#         if not test:
+#             flash("Hmm, we don't have that address...")
+#             return redirect('/')
+#         else:
+#             print(test.new_num)
+
 #     return render_template("index.html", form = form, newform=newform)
 
 
@@ -122,3 +107,31 @@ def no_go():
 @app.route("/contact")
 def contact_me():
     return render_template("contact.html")
+
+    # unused tessstsss
+    #below route is how to open the csvfile in a pandas dataframe
+# @app.route("/searchold", methods=["GET", "POST"])
+# def go_search():
+#     form = SearchOld()
+#     newform = SearchNew()
+#     oldnum = SearchOld(form.old_number.data)
+#     print("this is the oldnumber {}".format(oldnum))
+#     df = pd.read_csv('chicago\cacs_carmen_ave.csv')
+#         # reading = csv.DictReader(csvfile)
+#         # add_search = pd.DataFrame.from_dict(reading)
+#     print(df['old_num'][0])
+#     print(df[df['new_num'] == oldnum])
+#     for ad in df['old_num']:
+#         if ad == SearchOld(form.old_number.data):
+#             print('yes babnanas')
+#             break
+#         else:
+#             pass
+#     # testing = df[(df['old_num'] == oldnum)]
+#     # print(testing)
+#     # if str(oldnum) in str(add_search['old_num']):
+#     #     print("yes today")
+#     # else:
+#     #     print("we have no bananas today")
+
+#     return render_template("index.html", form = form, newform=newform)
