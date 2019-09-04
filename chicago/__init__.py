df250399a96f4ced98aca6b95bb2b97f29126b4c
[leylairoyale] <[leylairoyale@gmail.com]>
2019-09-04 10:21:39 -0500

    put logo in the middle

--- chicago/__init__.py
+++ chicago/__init__.py
@@ -10,5 +10,9 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
+# if you switch to elephantsql, then you will change the sqlite link below to the one that elephant
+# will give you so it will hook up to the db gthat way via sqlalchemy. joel recommends using this
+# over aws.
+
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///' + os.path.join(basedir, 'data.db')
 app.config['SECRET_KEY'] = "go do your research"
