3c7d5169e9a5b72868eecb6fc017210d84306d05
[leylairoyale] <[leylairoyale@gmail.com]>
2019-09-05 13:09:04 -0500

    Sorted things and css.

--- chicago/forms.py
+++ chicago/forms.py
@@ -6,12 +6,16 @@ class SearchOld(FlaskForm):
     old_number = StringField("Old Number")
     street_name = StringField("Street Name")
     direction = SelectField("Direction", choices=[('n', 'N'), ('s', 'S'), ('e', 'E'), ('w', 'W')])
-    old_street_type = SelectField("Street Type", choices=[('Street', 'Street'), ('Alley', 'Alley'), ('Lane', 'Lane'), ('Avenue', 'Avenue'), ('Place', 'Place'), ('Boulevard', 'Boulevard'), ('Way', 'Way'), ('Plaza', 'Plaza'), ('Drive', 'Drive'), ('Road', 'Road'), ('na', 'na')])
+    old_street_type = SelectField("Street Type", choices=sorted([('Street', 'Street'), ('Alley', 'Alley'), ('Lane', 'Lane'),
+     ('Avenue', 'Avenue'), ('Place', 'Place'), ('Boulevard', 'Boulevard'), ('Way', 'Way'),
+      ('Plaza', 'Plaza'), ('Drive', 'Drive'), ('Road', 'Road'), ('na', 'na')]))
     submit = SubmitField()
 
 class SearchNew(FlaskForm):
     new_number = StringField("New Number")
     street_name = StringField("Street Name")
     direction = SelectField("Direction", choices=[('n', 'N'), ('s', 'S'), ('e', 'E'), ('w', 'W')])
-    new_street_type = SelectField("Street Type", choices=[('Street', 'Street'), ('Avenue', 'Avenue'), ('Alley', 'Alley'), ('Lane', 'Lane'), ('Place', 'Place'), ('Boulevard', 'Boulevard'), ('Way', 'Way'), ('Plaza', 'Plaza'), ('Drive', 'Drive'), ('Road', 'Road'), ('na', 'na')])
+    new_street_type = SelectField("Street Type", choices=sorted([('Street', 'Street'), ('Avenue', 'Avenue'), ('Alley', 'Alley'), ('Lane', 'Lane'),
+     ('Place', 'Place'), ('Boulevard', 'Boulevard'), ('Way', 'Way'),
+      ('Plaza', 'Plaza'), ('Drive', 'Drive'), ('Road', 'Road'), ('na', 'na')]))
     submit = SubmitField()
